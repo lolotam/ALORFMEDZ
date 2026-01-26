59e731a36dc35ad0a3acc214e41da4426d2c44da
ALORFMEDZ Admin <admin@alorfmedz.com>
2026-01-26 06:20:28 +0300

    Fix mixed content

--- app/__init__.py
+++ app/__init__.py
@@ -60,7 +60,7 @@ def create_app(config_name=None):
                 static_folder='../static')
 
     # Fix for mixed content issues (HTTPS proxy)
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
     # Load configuration
     config_class = get_config(config_name)
